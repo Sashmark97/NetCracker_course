d22692d03ae84341bd9300f50bb8fb1325a649c9
ScarleT <Sashmark97@gmail.com>
2019-04-23 17:41:24 +0300

    patterns init

---
+++ Patterns/src/ru/skillbench/tasks/text/regex/Main.java
@@ -0,0 +1,13 @@
+package ru.skillbench.tasks.text.regex;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
